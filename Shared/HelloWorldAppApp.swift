7dc6369183ebcea129b630cf0bdfdf1d885132e0
Evgeniy Sapega <ntk_93@mail.ru>
2022-03-13 20:09:42 +0300

    Initial Commit

---
+++ Shared/HelloWorldAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldAppApp.swift
+//  Shared
+//
+//  Created by Evgeniy Sapega on 13.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
